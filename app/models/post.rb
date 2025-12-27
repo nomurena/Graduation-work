5193f664dc6cdd961221044c3a1fff83998dd19e
nomurena <retuma3@outlook.jp>
2025-12-27 18:41:07 +0900

    エラー文

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
     has_many_attached :images
-    validates :title, presence: true
-    validates :body, presence: true
+    validates :title, presence: { message: "タイトルを入力してください" }
+    validates :body, presence: { message: "メモを入力してください" }
 end
