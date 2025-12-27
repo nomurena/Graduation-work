1000f24b7c780853558fa6c9dad8a8b03d704500
nomurena <retuma3@outlook.jp>
2025-12-27 15:46:44 +0900

    アプリ１段階

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+  const mainImage = document.querySelector(".main-post-image");
+  const subImages = document.querySelectorAll(".sub-post-image");
+
+  if (!mainImage) return;
+
+  subImages.forEach((img) => {
+    img.addEventListener("click", () => {
+      mainImage.src = img.src;
+    });
+  });
+});
