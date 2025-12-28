263d1ea135b66d936e9bcfc86e590ca8833bd70c
nomurena <retuma3@outlook.jp>
2025-12-28 22:23:49 +0900

    拡張子の指定

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
     has_many_attached :images
     validates :title, presence: { message: "タイトルを入力してください" }
     validates :body, presence: { message: "メモを入力してください" }
+    validate :images_content_type
+
+    private
+
+  def images_content_type
+    images.each do |image|
+      unless image.content_type.in?(%w[image/jpeg image/png image/webp])
+        errors.add(:images, "はJPEG・PNG形式の画像のみ対応しています")
+      end
+    end
+  end
+
 end
